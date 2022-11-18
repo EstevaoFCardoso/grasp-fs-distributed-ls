45f8ea3c064e94cadb0cbad484e96e1de9bdaee3
Estêvão Filipe <estevaof10@gmail.com>
2022-11-18 16:42:46 -0300

    initial project commit

---
+++ Neighborhood/VND/grasp-fs-dls-vnd/src/main/java/br/com/graspfsdlsvnd/GraspFsDlsVndApplication.java
@@ -0,0 +1,13 @@
+package br.com.graspfsdlsvnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraspFsDlsVndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraspFsDlsVndApplication.class, args);
+	}
+
+}
