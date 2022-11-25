5ec83a76857a167b42ad3594cfd91acf2ddfc766
sequincozes <sequincozes@gmail.com>
2022-11-24 21:22:13 -0300

    Correções durante debug em código que será descartado.

--- Local Search/BitFlip/grasp-fs-dls-bf/src/main/java/br/com/graspfs/ls/bf/dto/DataSolution.java
+++ Local Search/BitFlip/grasp-fs-dls-bf/src/main/java/br/com/graspfs/ls/bf/dto/DataSolution.java
@@ -7,15 +7,15 @@ import lombok.Setter;
 
 import java.util.ArrayList;
 
-@Data
-@Builder
-@Getter
-@Setter
+//@Data
+//@Builder
+//@Getter
+//@Setter
 public class DataSolution {
 
     private  Long seedId;// id da mensagem
-    private final ArrayList<Integer> solutionFeatures;// []
-    private final ArrayList<Integer> rclfeatures;// []
+    private ArrayList<Integer> solutionFeatures;// []
+    private ArrayList<Integer> rclfeatures;// []
     private String neighborhood;// vnd
     private Integer iterationNeighborhood; //01
     private String classfier;// J48
@@ -23,6 +23,100 @@ public class DataSolution {
     private String localSearch; // BF
     private Long runnigTime;// tempo de execução
     private Integer iterationLocalSearch; //01
+
+    public Long getSeedId() {
+        return seedId;
+    }
+
+    public void setSeedId(Long seedId) {
+        this.seedId = seedId;
+    }
+
+    public ArrayList<Integer> getSolutionFeatures() {
+        return solutionFeatures;
+    }
+
+    public void setSolutionFeatures(ArrayList<Integer> solutionFeatures) {
+        this.solutionFeatures = solutionFeatures;
+    }
+
+    public ArrayList<Integer> getRclfeatures() {
+        return rclfeatures;
+    }
+
+    public void setRclfeatures(ArrayList<Integer> rclfeatures) {
+        this.rclfeatures = rclfeatures;
+    }
+
+    public String getNeighborhood() {
+        return neighborhood;
+    }
+
+    public void setNeighborhood(String neighborhood) {
+        this.neighborhood = neighborhood;
+    }
+
+    public Integer getIterationNeighborhood() {
+        return iterationNeighborhood;
+    }
+
+    public void setIterationNeighborhood(Integer iterationNeighborhood) {
+        this.iterationNeighborhood = iterationNeighborhood;
+    }
+
+    public String getClassfier() {
+        return classfier;
+    }
+
+    public void setClassfier(String classfier) {
+        this.classfier = classfier;
+    }
+
+    public Float getF1Score() {
+        return f1Score;
+    }
+
+    public void setF1Score(Float f1Score) {
+        this.f1Score = f1Score;
+    }
+
+    public String getLocalSearch() {
+        return localSearch;
+    }
+
+    public void setLocalSearch(String localSearch) {
+        this.localSearch = localSearch;
+    }
+
+    public Long getRunnigTime() {
+        return runnigTime;
+    }
+
+    public void setRunnigTime(Long runnigTime) {
+        this.runnigTime = runnigTime;
+    }
+
+    public Integer getIterationLocalSearch() {
+        return iterationLocalSearch;
+    }
+
+    public void setIterationLocalSearch(Integer iterationLocalSearch) {
+        this.iterationLocalSearch = iterationLocalSearch;
+    }
+
+    public DataSolution(Long seedId, ArrayList<Integer> solutionFeatures, ArrayList<Integer> rclfeatures, String neighborhood, Integer iterationNeighborhood, String classfier, Float f1Score, String localSearch, Long runnigTime, Integer iterationLocalSearch) {
+        this.seedId = seedId;
+        this.solutionFeatures = solutionFeatures;
+        this.rclfeatures = rclfeatures;
+        this.neighborhood = neighborhood;
+        this.iterationNeighborhood = iterationNeighborhood;
+        this.classfier = classfier;
+        this.f1Score = f1Score;
+        this.localSearch = localSearch;
+        this.runnigTime = runnigTime;
+        this.iterationLocalSearch = iterationLocalSearch;
+    }
+
 
 
 }
--- Local Search/BitFlip/grasp-fs-dls-bf/src/main/java/br/com/graspfs/ls/bf/service/BitFlipService.java
+++ Local Search/BitFlip/grasp-fs-dls-bf/src/main/java/br/com/graspfs/ls/bf/service/BitFlipService.java
@@ -1,73 +1,129 @@
 package br.com.graspfs.ls.bf.service;
 
-import br.com.graspfs.ls.bf.consumer.KafkaBitFlipConsumer;
 import br.com.graspfs.ls.bf.dto.DataSolution;
 import br.com.graspfs.ls.bf.producer.KafkaSolutionsProducer;
 import br.com.graspfs.ls.bf.util.LocalSearchUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Random;
 
-@Service
+//@Service
 public class BitFlipService {
 
-    private final Logger logg = LoggerFactory.getLogger(BitFlipService.class);
+    private static final Logger logg = LoggerFactory.getLogger(BitFlipService.class);
 
     KafkaSolutionsProducer kafkaSolutionsProducer;
 
-    public DataSolution flipFeatures(DataSolution solution, Long time){
+    public static void main(String[] args) {
+        ArrayList<Integer> seed = new ArrayList<>();
+
+        for (int i = 1; i <= 5; i++) {
+            seed.add(i);
+        }
+
+        ArrayList<Integer> rcl = new ArrayList<>();
+        for (int i = 6; i <= 20; i++) {
+            rcl.add(i);
+        }
+        rcl.add(21);
+        rcl.add(22);
+        rcl.add(23);
+        rcl.add(24);
+        rcl.add(25);
+
+        DataSolution ds = new DataSolution((long) 1,
+                seed,
+                rcl,
+                "VND",
+                1,
+                "J48",
+                0.50F,
+                "BitFlip",
+                500L,
+                1
+        );
+
+        flipFeatures(ds, 1L);
+    }
+
+
+    public static DataSolution flipFeatures(DataSolution solution, Long time) {
         var random = new Random();
         var valueIndex = 0;
         int i = 0;
         var positionReplace = 0;
-        DataSolution bestSolution= solution;
-        do{
+        DataSolution bestSolution = null;
+        do {
             Integer sumFeatures;
             valueIndex = random.nextInt(solution.getRclfeatures().size());
             positionReplace = random.nextInt(solution.getSolutionFeatures().size());
 
-            if(valueIndex>=0 && valueIndex<solution.getRclfeatures().size() && (positionReplace >= 0 &&  positionReplace<solution.getSolutionFeatures().size())){
+            if (valueIndex >= 0 && valueIndex < solution.getRclfeatures().size() && (positionReplace >= 0 && positionReplace < solution.getSolutionFeatures().size())) {
                 //bit flip
-                solution.getSolutionFeatures().add(positionReplace,solution.getRclfeatures().remove(valueIndex));
-                solution.getRclfeatures().add(valueIndex,solution.getRclfeatures().remove(positionReplace));
+                solution.getSolutionFeatures().add(solution.getRclfeatures().remove(valueIndex));
+                solution.getRclfeatures().add(solution.getSolutionFeatures().remove(positionReplace));
                 //soma metrica
                 sumFeatures = sumArray(solution.getSolutionFeatures());
                 solution.setF1Score(Float.valueOf(sumFeatures));
+                solution.setIterationLocalSearch(solution.getIterationLocalSearch() + 1);
+
                 //verifica bestSolution
-                solution.setRunnigTime(time-System.currentTimeMillis());
-                if(bestSolution.getF1Score() < solution.getF1Score()){
-                    bestSolution=solution;
+                solution.setRunnigTime(System.currentTimeMillis() - time);
+                if (bestSolution != null) {
+                    if (bestSolution.getF1Score() < solution.getF1Score()) {
+//                        bestSolution = solution;
+                        bestSolution = updateBestSolution(solution);
+                    }
+                } else {
+//                    bestSolution = solution;
+                    bestSolution = updateBestSolution(solution);
                 }
                 i++;
+                logg.info("Solution: " + solution.getF1Score() + " " + solution.getRunnigTime() + " " + solution.getNeighborhood() + " " + solution.getSolutionFeatures() + " "
+                        + solution.getIterationLocalSearch());
             }
-            logg.info("Message: "+ bestSolution.getF1Score() + " " + bestSolution.getRunnigTime() + " " + bestSolution.getNeighborhood() + " " + bestSolution.getSolutionFeatures() + " "
-                    + bestSolution.getIterationLocalSearch());
-        }while(i<100);
+
+
+        } while (i < 100);
+        logg.info("Best Solution: " + bestSolution.getF1Score() + " " + bestSolution.getRunnigTime() + " " + bestSolution.getNeighborhood() + " " + bestSolution.getSolutionFeatures() + " "
+                + bestSolution.getIterationLocalSearch());
         return bestSolution;
     }
 
-    public Integer sumArray(ArrayList<Integer> solution){
+    private static DataSolution updateBestSolution(DataSolution solution) {
+        return new DataSolution(solution.getSeedId(),
+                solution.getSolutionFeatures(),
+                solution.getRclfeatures(),
+                solution.getNeighborhood(),
+                solution.getIterationNeighborhood(),
+                solution.getClassfier(),
+                solution.getF1Score(),
+                solution.getLocalSearch(),
+                solution.getRunnigTime(),
+                solution.getIterationLocalSearch());
+    }
+
+    public static Integer sumArray(ArrayList<Integer> solution) {
         Integer sumMetric = 0;
-        for(int i = 0; i<solution.size()-1; i++){
+        for (int i = 0; i < solution.size() - 1; i++) {
             sumMetric = solution.get(i) + sumMetric;
         }
         return sumMetric;
     }
 
-    public void doBipFlip(DataSolution data, Long time){
+    public void doBipFlip(DataSolution data, Long time) {
         DataSolution bestSolution;
-        bestSolution = flipFeatures(data,time);
+        bestSolution = flipFeatures(data, time);
         bestSolution.setLocalSearch(LocalSearchUtils.BF);
-        bestSolution.setIterationLocalSearch(data.getIterationLocalSearch()+1);
+        bestSolution.setIterationLocalSearch(data.getIterationLocalSearch() + 1);
         bestSolution.setRunnigTime(time);
         bestSolution.setNeighborhood(data.getNeighborhood());
         bestSolution.getRclfeatures().addAll(data.getRclfeatures());
         bestSolution.setSeedId(data.getSeedId());
 
-        logg.info("BESTSOLUTION: "+ bestSolution.getF1Score() + " " + bestSolution.getRunnigTime() + " " + bestSolution.getNeighborhood() + " " + bestSolution.getSolutionFeatures() + " "
+        logg.info("BESTSOLUTION: " + bestSolution.getF1Score() + " " + bestSolution.getRunnigTime() + " " + bestSolution.getNeighborhood() + " " + bestSolution.getSolutionFeatures() + " "
                 + bestSolution.getIterationLocalSearch());
 
         kafkaSolutionsProducer.send(bestSolution);
